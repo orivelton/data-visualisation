c518aa5e8ffabb249924ca4ebaaa0ab3232a5d81
Orivelton Cesar <orivelton10@gmail.com>
2021-04-21 19:20:49 +0100

    chore: Docs and unit test

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# data-visualisation
-Data visualisation
---
+++ src/__tests__/api.test.js
@@ -0,0 +1,6 @@
+import { request } from '../helpers/api';
+
+test('Request function test', async () => {
+  const { current } = await request();
+  expect(current.data.TK1.message).toEqual('Prediction ok')
+});
